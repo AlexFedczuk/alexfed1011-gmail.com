4d6c060074c8af83c50d77f17f937e3f11697e89
AlexFedczuk <alexfed1011@gmail.com>
2020-03-25 19:23:24 -0300

    Lo practicado el miercoles 25/03/2020

--- S Ejercicio 2/E/main.c
+++ S Ejercicio 2/E/main.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main()
 {
-    char mensaje[30];
-    char letra;
+    float peso;
 
-    letra ='a';
-    printf("%c",letra);
+    printf("Ingrese su peso: ");
+    scanf("%f", &peso);
+    printf("Su peso es: %.2f",peso);
 
-    /*strcpy(mensaje,"Esto funciona de maravilla");
-    printf(mensaje);*/
     return 0;
+
+
 }
+/* int edad;
+
+    printf("Ingrese su edad: ");
+    scanf("%d", &edad);
+    printf("Su edad es: %d", edad);
+
+    return 0;*/
+
+/*  float pesoUsuario;
+
+    printf("Ingrese su peso: ");
+    scanf("%f", &pesoUsuario);
+    printf("Su peso es: %.2f", pesoUsuario);
+
+    return 0;*/
