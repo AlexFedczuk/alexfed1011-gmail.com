e89bcabe5ef111808951db3133372f22d0c4a98b
AlexFedczuk <alexfed1011@gmail.com>
2020-03-25 18:23:15 -0300

    Lo practicado el Miercoles 25/03/2020

---
+++ S Ejercicio 1/E/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char mensaje [30] = "Esto funciona de maravilla";
+
+    printf("%s \n", mensaje);
+    return 0;
+}//Esta bien!
+/*char mensaje[30];
+
+    printf("mensaje: ");
+    scanf("%s",&mensaje);
+    printf(mensaje);
+    return 0;*/
---
+++ S Ejercicio 2/E/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char mensaje[30];
+    char letra;
+
+    letra ='a';
+    printf("%c",letra);
+
+    /*strcpy(mensaje,"Esto funciona de maravilla");
+    printf(mensaje);*/
+    return 0;
+}
